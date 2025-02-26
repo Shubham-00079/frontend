5e78fa2a96be365fd96536f54da6fbb1f9a6a362
Shubham-00079 <shubham93014@gmail.com>
2025-02-26 10:55:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("https://repo-tech2edge.github.io/tasks/sample.json")
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById("data-container");
+            data.forEach(item => {
+                const card = document.createElement("div");
+                card.className = "col-md-4";
+                card.innerHTML = `
+                    <div class="card mb-4">
+                        <img src="${item.image}" class="card-img-top" alt="${item.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${item.title}</h5>
+                            <p class="card-text">${item.description}</p>
+                        </div>
+                    </div>
+                `;
+                container.appendChild(card);
+            });
+        })
+        .catch(error => console.error("Error fetching data:", error));
+});
