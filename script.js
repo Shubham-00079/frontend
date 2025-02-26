1f92cc0bbd77e424c011917e0d2575cb3c093ffd
Shubham-00079 <shubham93014@gmail.com>
2025-02-26 11:00:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,9 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Error fetching data:", error));
 });
+document.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG" && e.target.closest(".card")) {
+        document.getElementById("modalImage").src = e.target.src;
+        new bootstrap.Modal(document.getElementById("imageModal")).show();
+    }
+});
